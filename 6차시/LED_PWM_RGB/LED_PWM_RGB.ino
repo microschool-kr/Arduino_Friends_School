394be8ec2f339da5578bb2d7ff882169e760ef36
SeoMinKey <tjalsrl6751@gmail.com>
2022-12-16 14:42:38 +0900

    .

--- 6차시/LED_PWM_RGB/LED_PWM_RGB.ino
+++ 6차시/LED_PWM_RGB/LED_PWM_RGB.ino
@@ -5,7 +5,7 @@ void setup() // 여기에 코드를 입력하면 한 번만 실행합니다.
     pinMode(9, OUTPUT);
 }
 
-void loop() // 여기는 전원이 켜져있는 동안 무한대로 실행합니다.
+void loop() // 여기는 전원이 켜져 있는 동안 무한대로 실행합니다.
 {           // 핑크색을 만들기 위해 각 색깔별로 PWM 출력
     analogWrite(11, 212); // Red -> 212
     analogWrite(10, 76);  // Green -> 76
