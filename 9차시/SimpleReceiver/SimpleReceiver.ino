ac790a253b6a899b57610c2ddc433f22fb014b6e
AntonSangho <sanghoemail@gmail.com>
2023-02-20 18:12:53 +0900

    불필요한 define제거

--- 9차시/SimpleReceiver/SimpleReceiver.ino
+++ 9차시/SimpleReceiver/SimpleReceiver.ino
@@ -4,34 +4,12 @@
  ************************************************************************************
  */
 
-/*
- * 어떤 프로토콜을 사용할 것인지 정의하는 코드입니다.
- * 만약 어떤 프로토콜을 사용할지 모른다면, 아래 define의 모두 활성화시키면 됩니다. 
- * 이 코드는 #include <IRremote.hpp>전에 실행되어야 합니다.
- */
-#define DECODE_DENON
-#define DECODE_JVC
-#define DECODE_KASEIKYO
-#define DECODE_PANASONIC
-#define DECODE_LG
+// 적외선센서 프로토콜로 NEC로 사용할 것을 정의하는 코드입니다.
 #define DECODE_NEC
-#define DECODE_SAMSUNG
-#define DECODE_SONY
-#define DECODE_RC5
-#define DECODE_RC6
 
-#define DECODE_BOSEWAVE
-#define DECODE_LEGO_PF
-#define DECODE_MAGIQUEST
-#define DECODE_WHYNTER
-
-#define DECODE_DISTANCE_WIDTH
-#define DECODE_HASH
-
-
-#include <Arduino.h>  // 아두이노의 기본적이 기능을 담은 라이브러리입니다.
+#include <Arduino.h>  // 아두이노의 기본적이 기능을 담은 표준 라이브러리입니다.
 #include "PinDefinitionsAndMore.h"  // 컴퓨터에 연결된 아두이노에 따라 입력받을 Data핀을 결정하는 라이브러리 입니다
-#include <IRremote.hpp>  // 적외선 센서 기능을 사용할 수 있는 라이브러리입니다.
+#include <IRremote.hpp>  // 적외선 센서 기능을 담은 사용자 라이브러리입니다.
 
 void setup() {
   // 여기에 코드를 입력하면 한 번만 실행합니다.
@@ -42,7 +20,7 @@ void setup() {
   Serial.print(F("수신이 가능한 적외선 신호 프로토콜: "));     //시리얼 모니터에 프로토콜 안내문구를 출력합니다.
   printActiveIRProtocols(&Serial);                             //시리얼 모니터에 프로토콜 정보를 출력합니다.
   Serial.println(F("\nData pin번호는 " STR(IR_RECEIVE_PIN)));  //시리얼 모니터에 연결된 적외선 Data 핀 번호를 출력합니다.
-}
+
 
 void loop() {
   //여기는 전원이 켜져 있는 동안 반복실행합니다.
